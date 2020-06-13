dcf061318665996826527038a8dc0f25d53a6043
Mik0lai <44254049+Mik0lai@users.noreply.github.com>
2020-06-13 23:57:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return render_template("page.html", title="HOME PAGE")
+    return render_template("index.html", title="HOME PAGE")
 
 @app.route("/docs")
 def docs():
-    return render_template("page.html", title="docs page")
+    return render_template("index.html", title="docs page")
 
 @app.route("/about")
 def about():
-    return render_template("page.html", title="about page")
+    return render_template("index.html", title="about page")
 
 if __name__ == "__main__":
     app.run(debug=True)
